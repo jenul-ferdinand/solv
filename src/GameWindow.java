74bfcaca782094d619a51e9790f0ead5ebda8f16
Jenul <jenul15ferdinand@gmail.com>
2021-09-09 13:36:43 +1000

    added ubuntu fonts, setup jpanel and jframe.
    I implemented my custom fonts with java.io.File functions. I wanted to use google fonts to get the fonts over the cloud like you can with python but I'm not going to delve into this too much.
    
    Before this, I made the JFrame 1280x720, and changed the background colour of the JPanel to a dark grey resembling my GameMaker prototype. I used the function:
    
    main_window.setLocationRelativeTo(null);
    
    To make sure the window starts in the middle of the screen when launched.
    
    You may have noticed an unconventional variable naming convention I am using. I am using snake_case for my variables since I am used to it. I understand that this is not a good habit if I am working in a team since Java conventionally uses camelCase for their variables. It's ok, since there are indexing programs that can change variable names across files. I may change this habit after I become more fluent in Java. I'm always learning.

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,15 +1,34 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GameWindow {
     public static void main(String[] args) {
-        Game myCanvas = new Game();
-        myCanvas.init();
+        // Window dimensions
+        int window_width = 1280;
+        int window_height = 720;
 
-        JFrame window = new JFrame();
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setBounds(0, 0, 1280, 720);
-        window.getContentPane().add(myCanvas);
-        window.setVisible(true);
+        // Background color of the canvas
+        Color background_colour = new Color(28, 27, 31);
+
+        // Create the interface panel
+        MainPanel main_panel = new MainPanel();
+        main_panel.init();
+        main_panel.setBackground(background_colour); // Set the colour of the background
+
+        // Create the window frame
+        JFrame main_window = new JFrame("MadMath Idle");
+        main_window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        main_window.setBounds(0, 0, window_width, window_height); // Set the window dimensions
+        main_window.setLocationRelativeTo(null); // Center the window in the middle of the screen
+        main_window.getContentPane().add(main_panel); // Add the main panel inside the window
+        main_window.setVisible(true); // Draw visible
+
+        // Display all the fonts
+        String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+
+        for ( int i = 0; i < fonts.length; i++ ) {
+            System.out.println(fonts[i]);
+        }
     }
 }
 
