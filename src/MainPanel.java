0bbaa0f5e4e71a14db385bfe9ef46ecd3cbd5110
Jenul Ferdinand <jenul15ferdinand@gmail.com>
2021-10-05 20:43:12 +1100

    ✨ Further upgrades implementation, also back end components
    - Using the MouseMotionListener library to get mouseMoved method to find out if the mouse is hovering over the upgrade. Which is a fundamental step to create feedback so the interface follows the principles of usable design.
    
    - Fixed a bug that was causing lag when hovering over the button. Changing the image from the default button to the bright one was slow. This was because in the draw(Graphics g) method, in the try and catch block the images were being loaded infinitely which was causing lag.
    
    - Functionality to click on the button when you have enough to buy it. When you click, there is a flash effect which I created by drawing a white button image over the upgrade, then using AlphaComposite to reduce the alpha with linear interpolation.
    
    - Using cost increasing formula that most idle games use courtesy of Alexander King, and his research.
    
    - Using a Runnable class being targeted by a thread, which is run every second. Inside this, the marks per second will be added to the total marks. 
    
    - Fixed a bug being caused by null font variables causing NullPointerException bugs.
    
    - Cleaned up the structure of code in the MainPanel class.
    - Cleaned up the drawing text code in the MainPanel class.
    
    - The getStringHeight method does not need a String to be passed into it, this was fixed.
    
    - increaseTotalMarks method is deleted since total_marks is now a global variable.
    
    - Changed the draw method Graphics class attribute to Graphics2D since the draw method is being called by the MainPanel anyway, and Graphics2D is required for the getComposite function, otherwise we wouldn't be able to reset the AlphaComposite back to default.

--- src/MainPanel.java
+++ src/MainPanel.java
@@ -1,18 +1,19 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.geom.Area;
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 class MainPanel extends JPanel implements KeyListener{
 
     String keyboard_string = ""; // String the user has typed
     int char_max = 2; // Limit the amount of characters the user can type
     public static int total_marks = 0; // Points counting (marks)
-    int question_value = 1; // Value of the question (total_marks += question_value)
-    int marks_per_second = 0; // The idle addition variable
+    public static int question_value = 1; // Value of the question (total_marks += question_value)
+    public static int marks_per_second = 0; // The idle addition variable
     long questions_solved = 0; // Amount of questions solved (used for progression)
     int values_max = 5; // Random values range
     int value1 = -1; // First value
@@ -24,6 +25,9 @@ class MainPanel extends JPanel implements KeyListener{
     int right_window_buffer = GameWindow.window_width - 25;
     int bottom_window_buffer = GameWindow.window_height - 65;
 
+    int question_area_ypos = 300;
+    int answer_area_ypos = 370;
+
     // Fonts
     Font ubuntu_font;
     float question_fontsize = 72f;
@@ -34,6 +38,9 @@ class MainPanel extends JPanel implements KeyListener{
     int upgrade_ystart = 110;
     int upgrade_x = 990;
     int upgrade_gap = 12;
+
+    // Debug
+    boolean developer_mode = true;
 
     // Upgrade array containing all of the class types.
     Upgrade[] upgrade = {
@@ -55,14 +62,10 @@ class MainPanel extends JPanel implements KeyListener{
 
         // Register and create the font and then store it as a variable Font class.
         try {
-            ubuntu_font = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/Ubuntu-R.ttf"));
+            if (ubuntu_font == null) ubuntu_font = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/Ubuntu-R.ttf"));
             GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
             ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("fonts/Ubuntu-R.ttf")));
-        }
-        catch (IOException | FontFormatException e) {
-            System.out.println("can't process fonts");
-        }
-
+        } catch (IOException | FontFormatException e) {}
 
         // Setup the keyboard listener.
         addKeyListener(this);
@@ -72,11 +75,23 @@ class MainPanel extends JPanel implements KeyListener{
         // Randomise values
         randomiseValues();
 
-        // Add a mouse listener to each of the upgrades
+        // Add a mouse listeners to each of the upgrades
         for (int i = 0; i < upgrade.length; i++) {
             addMouseListener(upgrade[i]);
+            addMouseMotionListener(upgrade[i]);
             System.out.println("added mouse listener to upgrade " + i);
         }
+
+        // Runnable thread every second
+        // https://stackoverflow.com/questions/12908412/print-hello-world-every-x-seconds
+        Runnable everySecond = new Runnable() {
+            public void run() {
+                total_marks += marks_per_second;
+            }
+        };
+
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleAtFixedRate(everySecond, 0, 1, TimeUnit.SECONDS);
     }
 
     // Drawing on the panel
@@ -89,48 +104,38 @@ class MainPanel extends JPanel implements KeyListener{
         g2d.setRenderingHints(rh);
 
 
+        // Draw all the upgrades
+        // it will only be drawn if the upgrade object is flagged as displayed.
+        for (int i = 0; i < upgrade.length; i++) {
+            upgrade[i].draw((Graphics2D) g);
+        }
+
+
         // Set the sizes of the fonts that are going to be used
         Font question_font = ubuntu_font.deriveFont(question_fontsize);
         Font marks_font = ubuntu_font.deriveFont(marks_fontsize);
         Font statistics_font = ubuntu_font.deriveFont(statistics_fontsize);
 
-
-        //region Drawing Text
-
+        // Drawing text
         g.setColor(Color.WHITE);
         String str;
         // Total marks
         g.setFont(marks_font);
-        str = total_marks + " Marks";
-        g.drawString(str, left_window_buffer, top_window_buffer + (getStringHeight(g, marks_font, str)/2));
+        g.drawString(total_marks + " Marks", left_window_buffer, top_window_buffer + (getStringHeight(g, marks_font)));
         // Question value
         g.setFont(statistics_font);
-        str = "Question Value: " + question_value;
-        g.drawString(str, left_window_buffer, (top_window_buffer + (getStringHeight(g, statistics_font, str)/2)) + getStringHeight(g, marks_font, str)); // Draw under the marks
+        g.drawString("Question Value: " + question_value, left_window_buffer, (top_window_buffer + (getStringHeight(g, statistics_font))) + getStringHeight(g, marks_font) + 10);
         // Marks per second
-        str = "Marks Per Second: " + marks_per_second;
-        g.drawString(str, left_window_buffer, (top_window_buffer + (getStringHeight(g, statistics_font, str)/2)) + getStringHeight(g, marks_font, str) + getStringHeight(g, statistics_font, str) + 10);
+        g.drawString("Marks Per Second: " + marks_per_second, left_window_buffer, (top_window_buffer + (getStringHeight(g, statistics_font))) + getStringHeight(g, marks_font) + getStringHeight(g, statistics_font) + 20);
         // Question area
         g.setFont(question_font);
-        str = value1 + " + " + value2;
-        g.drawString(str, left_window_buffer, 300 + (getStringHeight(g, question_font, str)/2));
+        g.drawString(value1 + " + " + value2, left_window_buffer, question_area_ypos + (getStringHeight(g, question_font)));
         // Answer area
-        str = "= " + keyboard_string;
-        g.drawString(str, left_window_buffer, 370 + (getStringHeight(g, question_font, str)/2));
-
-        //endregion
-
-
-        // Draw all of the upgrades,
-        // it will only be drawn if the upgrade is displayed on the interface
-        for (int i = 0; i < upgrade.length; i++) {
-            upgrade[i].draw(g);
-        }
+        g.drawString("= " + keyboard_string, left_window_buffer, answer_area_ypos + (getStringHeight(g, question_font)));
 
 
         // Handle update to the paint cycle
         repaint();
-
     }
 
     //region Key Listener Interface Methods
@@ -152,7 +157,7 @@ class MainPanel extends JPanel implements KeyListener{
 
         }
 
-        // Deleting a char from the keyboard string
+        // Deleting characters
         if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
             if (!keyboard_string.equals("")) {
 
@@ -165,32 +170,48 @@ class MainPanel extends JPanel implements KeyListener{
 
         }
 
-        // Closing the game
-        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
-            System.exit(0); // Close game
-        }
-
-        // Submission of answer
+        // Parsing
         if (e.getKeyCode() == KeyEvent.VK_ENTER) {
 
-            // If answer is correct
-            if (Integer.parseInt(keyboard_string) == value1 + value2) {
+            // There must be text typed in
+            if (!keyboard_string.equals("")) {
 
-                keyboard_string = ""; // Clear the keyboard string
-                total_marks += question_value; // Add to the total_marks
-                questions_solved++; // Increment the questions solved
 
-                randomiseValues(); // Using this function we can get new random values to solve.
-                createUpgrades(); // Run the function to check if we should show a new upgrade.
+                // If answer is correct
+                if (Integer.parseInt(keyboard_string) == value1 + value2) {
 
-                e.consume(); // Stop handling
+                    keyboard_string = ""; // Clear the keyboard string
+                    total_marks += question_value; // Add to the total_marks
+                    questions_solved++; // Increment the questions solved
 
-                System.out.println("User submitted the correct answer"); // Debug message
+                    randomiseValues(); // Using this function we can get new random values to solve.
+                    createUpgrades(); // Run the function to check if we should show a new upgrade.
+
+                    e.consume(); // Stop handling
+
+                    System.out.println("Correct");
+
+                } else {
+
+                    System.out.println("Wrong");
+                }
+
+            } else {
+
+                System.out.println("No text");
             }
-
         }
 
+        // Closing the game
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            System.out.println("Closing game");
+            System.exit(0);
+        }
 
+        if (developer_mode && e.getKeyCode() == KeyEvent.VK_EQUALS) {
+            total_marks += 100000;
+            createUpgrades();
+        }
     }
     public void keyTyped(KeyEvent e) {}
     public void keyReleased(KeyEvent e) {}
@@ -203,7 +224,7 @@ class MainPanel extends JPanel implements KeyListener{
         for (int i = 0; i < upgrade.length; i++) {
 
             // Displaying the upgrade if the player has progressed at or past the creation
-            if (total_marks >= upgrade[i].creation) {
+            if (total_marks >= upgrade[i].creation && !upgrade[i].displayed)  {
                 int yy;
 
                 if (i == 0) {
@@ -239,15 +260,10 @@ class MainPanel extends JPanel implements KeyListener{
     }
 
     // Return the height (pixels) of an inputted string.
-    public static int getStringHeight(Graphics page, Font f, String s) {
-        // Find the size of string s in the font of the Graphics context "page"
+    public static int getStringHeight(Graphics page, Font f) {
         FontMetrics fm = page.getFontMetrics(f);
-        // Return the value.
         return fm.getAscent();
     }
 
-    public static void increaseTotalMarks(int amount) {
-        total_marks += amount;
-    }
 
 }
--- src/Upgrade.java
+++ src/Upgrade.java
@@ -3,110 +3,182 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.geom.Area;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.awt.Graphics2D;
 
-public class Upgrade extends JPanel implements MouseListener {
+public class Upgrade extends JPanel implements MouseListener, MouseMotionListener {
 
+    // Properties
     boolean displayed = false;
     int x = 990;
     int y = 110;
+    int quantity = 0;
 
+    // Images
     BufferedImage button_image;
+    BufferedImage button_bright_image;
+    BufferedImage button_white_image;
+    BufferedImage button_displayed_image;
     int button_height;
     int button_width;
+    Area button_area;
+    float flash_alpha = 0f;
 
     BufferedImage icon_image;
     int icon_width;
     int icon_height;
 
-    String name;
-    int creation;
-    long cost;
-    String desc;
-    String icon_path;
-
+    // Font
+    Font ubuntu_font;
+    Font label_font;
+    float label_fontsize = 20f;
+    // Attributes initialisation
+    String name, desc, icon_path;
+    int creation, mps, qv;
+    long cost, base_cost;
+    // Constructor
     public Upgrade(String name, String desc, long cost, int mps, int qv, int creation, String icon_path) {
-        this.name = name; // Name of the upgrade
-        this.creation = creation; // This is how many marks the player should have to make the upgrade visible
-        this.cost = cost; // Cost of the upgrade
-        this.desc = desc; // Description that will be on the upgrade's tooltip
+        this.name = name;           // Name of the upgrade
+        this.creation = creation;   // This is how many marks the player should have to make the upgrade visible
+        this.cost = cost;           // Cost of the upgrade
+        this.base_cost = cost;      // Base cost of the upgrade
+        this.mps = mps;             // Marks per second benefit
+        this.qv = qv;               // Question value benefit
+        this.desc = desc;           // Description that will be on the upgrade's tooltip
         this.icon_path = icon_path; // The file path for the icon image
     }
 
     // Drawing the upgrade and it's contents
-    public void draw(Graphics g) {
+    public void draw(Graphics2D g) {
 
-        // Image management
+        // Image & font management
         try {
-
             // Store the button image in variable
-            button_image = ImageIO.read(new File("images/upgrade-button.png"));
+            if (button_image == null) button_image = ImageIO.read(new File("images/upgrade-button.png"));
+            if (button_bright_image == null) button_bright_image = ImageIO.read(new File("images/upgrade-button-bright.png"));
+            if (button_white_image == null) button_white_image = ImageIO.read(new File("images/upgrade-button-white.png"));
             // Get height and width of the button
             button_height = button_image.getHeight();
             button_width = button_image.getWidth();
 
             // Store the icon image in variable
-            icon_image = ImageIO.read(new File("images/" + icon_path));
+            if (icon_image == null) icon_image = ImageIO.read(new File("images/" + icon_path));
             // Get height and width of the icon
             icon_height = icon_image.getHeight();
             icon_width = icon_image.getWidth();
 
-        } catch (IOException e) {
-            // Debug
-            System.out.println("Image loading failed.");
-        }
+            // Fonts
+            if (ubuntu_font == null) ubuntu_font = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/Ubuntu-R.ttf"));
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("fonts/Ubuntu-R.ttf")));
+        } catch (IOException | FontFormatException e) { }
 
 
 
-        // If this upgrade has been displayed on the screen
+        // Visible drawing elements
         if (displayed) {
+
+            if (ubuntu_font != null) label_font = ubuntu_font.deriveFont(label_fontsize);
+
             // Draw the button
-            g.drawImage(button_image, x, y,this);
+            g.drawImage(button_displayed_image, x, y,this);
 
             // Draw the icon
-            g.drawImage(icon_image, x, y, this);
-        }
+            g.drawImage(icon_image, x + 10, y + 10, this);
 
+            // Draw the details
+            if (ubuntu_font != null) {
+                g.setFont(label_font);
+                g.drawString(name + ": " + quantity, x + icon_width + 15, y + getStringHeight(g, label_font) + 10);
+            }
+
+
+
+            // Draw the flash image with alpha composite implemented
+            // https://www.daniweb.com/programming/software-development/threads/358686/setting-image-opacity
+            Composite default_composite = g.getComposite();
+            AlphaComposite alpha_composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, flash_alpha);
+            g.setComposite(alpha_composite);
+            g.drawImage(button_white_image, x, y, this);
+            g.setComposite(default_composite);
+
+
+
+            // Linear interpolate alpha back to zero
+            if (flash_alpha > 0) {
+                flash_alpha = lerp(flash_alpha, 0f, 0.085f);
+            } else {
+                flash_alpha = 0;
+            }
+        }
 
     }
 
+    private int getStringHeight(Graphics page, Font f) {
+        FontMetrics fm = page.getFontMetrics(f);
+        return fm.getAscent();
+    }
 
 
+    //region Mouse Interface
     public void mousePressed(MouseEvent e) {
 
         if (displayed) {
-
-            // Area checking vars
-            Rectangle temp_rect = new Rectangle(x, y, button_width, button_height);
-            Area button_area = new Area(temp_rect);
-
-            // If the mouse was over the upgrade
+            // Set the area of the button
+            button_area = new Area(new Rectangle(x, y, button_width, button_height));
+            // If the mouse is over the upgrade
             if (button_area.contains(e.getX(), e.getY())) {
+                // If the user has enough to purchase
+                if  (MainPanel.total_marks >= cost) {
 
-                // Do total marks checking here
+                    MainPanel.total_marks -= cost;
 
+                    MainPanel.marks_per_second += mps;
+                    MainPanel.question_value += qv;
 
-                // Debug
-                System.out.println("upgrade y: " + y);
-                MainPanel.increaseTotalMarks(20);
+                    quantity++;
+
+                    flash_alpha = 1f;
+
+                    // Increase the cost using cookie clicker formula, this is fundamental for difficulty.
+                    // https://gamedevelopment.tutsplus.com/articles/numbers-getting-bigger-the-design-and-math-of-incremental-games--cms-24023
+                    double formula = base_cost * Math.pow(1.15, quantity);
+                    cost = (long) formula;
+
+                    // Debug
+                    System.out.println(name + " upgrade purchased!");
+                }
+
             }
 
         }
 
     }
-    public void mouseClicked(MouseEvent e) {
+    public void mouseClicked(MouseEvent e) {}
+    public void mouseReleased(MouseEvent e) {}
+    public void mouseEntered(MouseEvent e) {}
+    public void mouseExited(MouseEvent e) {}
+    public void mouseDragged(MouseEvent e) {}
+    public void mouseMoved(MouseEvent e) {
+        // Set the area of the button
+        button_area = new Area(new Rectangle(x, y, button_width, button_height));
+        // If the mouse is over the upgrade and displayed
+        if (button_area.contains(e.getX(), e.getY()) && displayed) {
+            // Change displayed button image to the bright version
+            button_displayed_image = button_bright_image;
+        }
+        else {
+            // Reset back to default image
+            button_displayed_image = button_image;
+        }
+    }
+    //endregion
 
-    }
-    public void mouseReleased(MouseEvent e) {
-
-    }
-    public void mouseEntered(MouseEvent e) {
-        // Lighten the button image
-    }
-    public void mouseExited(MouseEvent e) {
-        // Reset the button image
+    public float lerp(float a, float b, float f) {
+        return a + f * (b - a);
     }
 }
